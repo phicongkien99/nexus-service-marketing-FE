363dc20f76353255a0d72584625bde642ea5da1f
unknown <biprono12@gmail.com>
2020-10-28 20:58:41 +0700

    commit

--- src/app/admin/dashboard/index.js
+++ src/app/admin/dashboard/index.js
@@ -1,8 +1,33 @@
+import { Card, Col, Row } from "antd";
 import React from "react";
+import "./Dashboard.scss";
 
 function Dashboard(props) {
     return (
-        <div>Dashboard</div>
+        <div id="admin-dashboard">
+            <Row gutter={15}>
+                <Col span={6}>
+                    <Card className="card-stat" title="Placeholder">
+                        Wassup
+                    </Card>
+                </Col>
+                <Col span={6}>
+                    <Card className="card-stat" title="Placeholder">
+                        Wassup
+                    </Card>
+                </Col>
+                <Col span={6}>
+                    <Card className="card-stat" title="Placeholder">
+                        Wassup
+                    </Card>
+                </Col>
+                <Col span={6}>
+                    <Card className="card-stat" title="Placeholder">
+                        Wassup
+                    </Card>
+                </Col>
+            </Row>
+        </div>
     )
 }
 
