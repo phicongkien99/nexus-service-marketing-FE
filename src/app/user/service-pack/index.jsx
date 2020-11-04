bd0d55718679116876eda455e021f7908ad54f90
phí kiên <44530636+PhiKien@users.noreply.github.com>
2020-11-04 12:45:25 +0700

    update service pack page

--- src/app/user/service-pack/index.jsx
+++ src/app/user/service-pack/index.jsx
@@ -11,6 +11,7 @@ function ServicePack(props) {
 
     const nameServicePackStyle = {
         textAlign: "center",
+        justifyContent: "center"
     }
 
     const itemServicePackStyle = {
@@ -21,7 +22,7 @@ function ServicePack(props) {
         // marginRight:"1px",
     }
 
-    const buttonStyle = {
+    const buttonOrangeStyle = {
         border: "2px solid #FF7318",
         borderRadius: "86px",
         fontStyle: "normal",
@@ -33,8 +34,33 @@ function ServicePack(props) {
         marginTop: "20px"
     }
 
+    const buttonBlueStyle = {
+        border: "2px solid #035690",
+        borderRadius: "86px",
+        fontStyle: "normal",
+        fontWeight: "600",
+        fontSize: "18px",
+        lineHeight: "23px",
+        textAlign: "center",
+        color: "#035690",
+        marginTop: "20px"
+    }
+
+    const buttonGreenStyle = {
+        border: "2px solid #62b046",
+        borderRadius: "86px",
+        fontStyle: "normal",
+        fontWeight: "600",
+        fontSize: "18px",
+        lineHeight: "23px",
+        textAlign: "center",
+        color: "#62b046",
+        marginTop: "20px"
+    }
+
     return (
         <>
+            {/* Slide top */}
             <Row >
                 <Col span={16} offset={4}>
                     <Row gutter={5}>
@@ -69,8 +95,9 @@ function ServicePack(props) {
 
                 </Col>
             </Row>
-            <Row className="mt-10" gutter={5}>
-                <Col span={16} offset={4}>
+            {/*   Dial-up  */}
+            <Row className="mt-10" style={{ justifyContent: "center" }} >
+                <Col span={16}>
                     <Row>
                         <Col span={8}>
                         </Col>
@@ -83,7 +110,7 @@ function ServicePack(props) {
                         <Col span={8}>
                         </Col>
                     </Row>
-                    <Row className="mt-10" style={nameServicePackStyle} gutter={5}>
+                    <Row className="mt-10" style={nameServicePackStyle} >
                         <Col style={itemServicePackStyle} span={8}>
                             <h4 style={{ marginBottom: "0" }}>COMBO</h4>
                             <h1>
@@ -106,7 +133,7 @@ function ServicePack(props) {
                                 <br />
                                 Technical support 24/7
                             </p>
-                            <Button style={buttonStyle}>Register now</Button>
+                            <Button style={buttonOrangeStyle}>Register now</Button>
                         </Col>
                         <Col style={itemServicePackStyle} span={8}>
                             <h4 style={{ marginBottom: "0" }}>COMBO</h4>
@@ -130,11 +157,12 @@ function ServicePack(props) {
                                 <br />
                                 Technical support 24/7
                             </p>
-                            <Button style={buttonStyle}>Register now</Button>
+                            <Button style={buttonOrangeStyle}>Register now</Button>
                         </Col>
                         <Col style={itemServicePackStyle} span={8}>
+                            <h4 style={{ marginBottom: "0" }}>COMBO</h4>
                             <h1>
-                                <b style={{ fontSize: "30px" }} className="text-orange">UNLIMITED 56K</b>
+                                <b style={{ fontSize: "30px" }} className="text-orange">UNLIMITED 56Kbps</b>
                             </h1>
                             <span>From</span>
                             <p>110 $/month</p>
@@ -153,9 +181,249 @@ function ServicePack(props) {
                                 <br />
                                 Technical support 24/7
                             </p>
-                            <Button style={buttonStyle}>Register now</Button>
+                            <Button style={buttonOrangeStyle}>Register now</Button>
                         </Col>
-
+                    </Row>
+                </Col>
+            </Row>
+            {/*   Boad Band  */}
+            <Row className="mt-20" style={{ justifyContent: "center" }} >
+                <Col span={16}>
+                    <Row>
+                        <Col span={8}>
+                        </Col>
+                        <Col style={nameServicePackStyle} span={8}>
+                            <Row style={{ backgroundColor: "#4EB848", justifyContent: "center", borderRadius: "52px" }}>
+                                <Image className="mt-8" src={"https://fpt.vn/shop/html/assets/images/icon/icon-net-only.png"} />
+                                <h2 className="mt-3" style={{ color: "white" }}>Boad Band</h2>
+                            </Row>
+                        </Col>
+                        <Col span={8}>
+                        </Col>
+                    </Row>
+                    <Row className="mt-10" style={nameServicePackStyle} >
+                        <Col style={itemServicePackStyle} span={8}>
+                            <h4 style={{ marginBottom: "0" }}>COMBO</h4>
+                            <h1>
+                                <b style={{ fontSize: "30px" }} className="text-green">HOURLY BASIS</b>
+                            </h1>
+                            <span>From</span>
+                            <p>100 $/month</p>
+                            <p style={{ color: "#BDBDBD" }}>Price does not include VAT</p>
+                            <span>Download / Upload</span>
+                            <p><b><h2>25 Mbps</h2></b></p>
+                            <hr style={{ width: "50%", color: "#BDBDBD" }} />
+                            <p>
+                                More than 180 TV channels
+                                <br /><br />
+                                Free installation *
+                                <br />
+                                Equipped with WiFi Modem & HD Box
+                                <br /><br />
+                                Fast installation in 12 hours
+                                <br />
+                                Technical support 24/7
+                            </p>
+                            <Button style={buttonGreenStyle}>Register now</Button>
+                        </Col>
+                        <Col style={itemServicePackStyle} span={8}>
+                            <h4 style={{ marginBottom: "0" }}>COMBO</h4>
+                            <h1>
+                                <b style={{ fontSize: "30px" }} className="text-green">UNLIMITED 64Kbps</b>
+                            </h1>
+                            <span>From</span>
+                            <p>110 $/month</p>
+                            <p style={{ color: "#BDBDBD" }}>Price does not include VAT</p>
+                            <span>Download / Upload</span>
+                            <p><b><h2>25 Mbps</h2></b></p>
+                            <hr style={{ width: "50%", color: "#BDBDBD" }} />
+                            <p>
+                                More than 180 TV channels
+                                <br /><br />
+                                Free installation *
+                                <br />
+                                Equipped with WiFi Modem & HD Box
+                                <br /><br />
+                                Fast installation in 12 hours
+                                <br />
+                                Technical support 24/7
+                            </p>
+                            <Button style={buttonGreenStyle}>Register now</Button>
+                        </Col>
+                        <Col style={itemServicePackStyle} span={8}>
+                            <h4 style={{ marginBottom: "0" }}>COMBO</h4>
+                            <h1>
+                                <b style={{ fontSize: "30px" }} className="text-green">UNLIMITED 128Kbps</b>
+                            </h1>
+                            <span>From</span>
+                            <p>110 $/month</p>
+                            <p style={{ color: "#BDBDBD" }}>Price does not include VAT</p>
+                            <span>Download / Upload</span>
+                            <p><b><h2>25 Mbps</h2></b></p>
+                            <hr style={{ width: "50%", color: "#BDBDBD" }} />
+                            <p>
+                                More than 180 TV channels
+                                <br /><br />
+                                Free installation *
+                                <br />
+                                Equipped with WiFi Modem & HD Box
+                                <br /><br />
+                                Fast installation in 12 hours
+                                <br />
+                                Technical support 24/7
+                            </p>
+                            <Button style={buttonGreenStyle}>Register now</Button>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+            {/*  Land Line  */}
+            <Row className="mt-20" style={{ justifyContent: "center" }}>
+                <Col span={16}>
+                    <Row>
+                        <Col span={8}>
+                        </Col>
+                        <Col style={nameServicePackStyle} span={8}>
+                            <Row style={{ backgroundColor: "#035690", justifyContent: "center", borderRadius: "52px" }}>
+                                <Image className="mt-8" src={"https://fpt.vn/shop/html/assets/images/icon/icon-combo.png"} />
+                                <h2 className="mt-3" style={{ color: "white" }}>Land Line</h2>
+                            </Row>
+                        </Col>
+                        <Col span={8}>
+                        </Col>
+                    </Row>
+                    <Row className="mt-10" style={nameServicePackStyle} >
+                        <Col style={itemServicePackStyle} span={8}>
+                            <h4 style={{ marginBottom: "0" }}>COMBO</h4>
+                            <h1>
+                                <b style={{ fontSize: "30px" }} className="text-blue">LOCAL PLAN</b>
+                            </h1>
+                            <span>From</span>
+                            <p>100 $/month</p>
+                            <p style={{ color: "#BDBDBD" }}>Price does not include VAT</p>
+                            <span>Download / Upload</span>
+                            <p><b><h2>25 Mbps</h2></b></p>
+                            <hr style={{ width: "50%", color: "#BDBDBD" }} />
+                            <p>
+                                More than 180 TV channels
+                                <br /><br />
+                                Free installation *
+                                <br />
+                                Equipped with WiFi Modem & HD Box
+                                <br /><br />
+                                Fast installation in 12 hours
+                                <br />
+                                Technical support 24/7
+                            </p>
+                            <Button style={buttonBlueStyle}>Register now</Button>
+                        </Col>
+                        <Col style={itemServicePackStyle} span={8}>
+                            <h4 style={{ marginBottom: "0" }}>COMBO</h4>
+                            <h1>
+                                <b style={{ fontSize: "30px" }} className="text-blue">STD PLAN</b>
+                            </h1>
+                            <span>From</span>
+                            <p>110 $/month</p>
+                            <p style={{ color: "#BDBDBD" }}>Price does not include VAT</p>
+                            <span>Download / Upload</span>
+                            <p><b><h2>25 Mbps</h2></b></p>
+                            <hr style={{ width: "50%", color: "#BDBDBD" }} />
+                            <p>
+                                More than 180 TV channels
+                                <br /><br />
+                                Free installation *
+                                <br />
+                                Equipped with WiFi Modem & HD Box
+                                <br /><br />
+                                Fast installation in 12 hours
+                                <br />
+                                Technical support 24/7
+                            </p>
+                            <Button style={buttonBlueStyle}>Register now</Button>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+            {/* Promotion Service Pack */}
+            <Row className="mt-20" style={{ justifyContent: "center" }}>                
+                <Col span={16}>
+                    {/* Image left */}
+                    <Row>
+                        <Row style={{ fontSize: "30px", justifyContent: "center" }}>
+                            <h1><b>Special offer for Online Subscription only</b></h1>
+                        </Row>
+                        <Row>
+                            <Col span={16}>
+                                <img style={{ width: "900px" }} src="https://fpt.vn/shop/html/assets/images/event_t11/desktop/5.jpg" />
+                            </Col>
+                            <Col span={8} style={{ paddingLeft: "20px" }}>
+                                <Row>
+                                    <h1 className="text-orange front-size-20" style={{ marginLeft: "4%" }}>Apply only at Nexus Service</h1>
+                                </Row>
+                                <Row>
+                                    <ul>
+                                        <li>
+                                            Up to 3 months free when prepaid Hourly Basis package
+                                        </li>
+                                        <li>
+                                            Up to 2 months free when prepaid Unlimited 24K package
+                                        </li>
+                                        <li>
+                                            Up to 1 months free when prepaid Unlimited 56Kbps package
+                                         </li>
+                                    </ul>
+                                </Row>
+                            </Col>
+                        </Row>
+                    </Row>
+                    {/* Image right */}
+                    <Row className="mt-20">
+                        <Row>
+                            <Col span={8}>
+                                <Row>
+                                    <h1 className="text-orange front-size-20" style={{ marginLeft: "4%" }}>Apply only at Nexus Service</h1>
+                                </Row>
+                                <Row>
+                                    <ul>
+                                        <li>
+                                            Up to 3 months free when prepaid Hourly Basis package
+                                        </li>
+                                        <li>
+                                            Up to 2 months free when prepaid Unlimited 64Kbps package
+                                        </li>
+                                        <li>
+                                            Up to 1 months free when prepaid Unlimited 120Kbps package
+                                        </li>
+                                    </ul>
+                                </Row>
+                            </Col>
+                            <Col span={16}>
+                                <img style={{ width: "900px" }} src="https://fpt.vn/shop/html/assets/images/event_t11/desktop/6.png" />
+                            </Col>
+                        </Row>
+                    </Row>
+                    {/* Image left */}
+                    <Row className="mt-20">                        
+                        <Row>
+                            <Col span={16}>
+                                <img style={{ width: "900px" }} src="https://fpt.vn/shop/html/assets/images/event_t11/desktop/5.jpg" />
+                            </Col>
+                            <Col span={8} style={{ paddingLeft: "42px" }}>
+                                <Row>
+                                    <h1 className="text-orange front-size-20" style={{ marginLeft: "4%" }}>Apply only at Nexus Service</h1>
+                                </Row>
+                                <Row>
+                                    <ul>
+                                        <li>
+                                            Up to 3 months free when prepaid Local Plan package
+                                        </li>
+                                        <li>
+                                            Up to 2 months free when prepaid STD package
+                                        </li>                                        
+                                    </ul>
+                                </Row>
+                            </Col>
+                        </Row>
                     </Row>
                 </Col>
             </Row>
