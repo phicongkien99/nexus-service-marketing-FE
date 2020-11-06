7776ecc12eb5a490350f85fc7268e1b848efe493
phí kiên <44530636+PhiKien@users.noreply.github.com>
2020-11-07 00:09:31 +0700

    update search contract

--- src/app/user/membership/index.jsx
+++ src/app/user/membership/index.jsx
@@ -1,17 +1,87 @@
 import React, { useRef } from "react";
-import { Row, Col } from "antd";
-import { Carousel, Button, Image } from "antd";
+import { Row, Col, Input, Image, Button } from "antd";
+import { AudioOutlined } from '@ant-design/icons';
+import { Link } from "react-router-dom";
+import { BookTwoTone } from "@ant-design/icons";
 
 function Membership(props) {
 
+    const { Search } = Input;
+
+    const suffix = (
+        <AudioOutlined
+            style={{
+                fontSize: 16,
+                color: '#1890ff',
+            }}
+        />
+    );
+
+    const onSearch = value => console.log(value);
+
+
     return (<>
-        <Row style={{ justifyContent: "center" }}>
-            <Col span={16}>
-                <Row style={{ justifyContent: "left" }}>
-                    <Row>
-                        <Image height={156} width src={"https://id.fpt.vn/images/logo.png"}/>
-                    </Row>
-                    <Row></Row>
+        <Row className="mt-20" style={{ justifyContent: "center" }}>
+            <Col span={16} offset={8}>
+                <Row>
+                    <Image width={156} height={52} src={"https://id.fpt.vn/images/logo.png"} />
+                </Row>
+                <Row className="mt-10">
+                    <h4 className="front-size-30">LOOK UP CONTRACT</h4>
+                </Row>
+                <Row className="mt-3">
+                    <label className="front-size-15">Your Nexus-Service contract code</label>
+                </Row>
+                <Row className="mt-20">
+                    <Col span={8}>
+                        <Search
+                            placeholder="input your contract code"
+                            allowClear
+                            enterButton="Search"
+                            size="large"
+                            onSearch={onSearch}
+                        />
+                    </Col>
+                </Row>
+                <Row className="mt-20">
+                    <Col span={8}>
+                        <hr />
+                    </Col>
+                </Row>
+                <Row className="mt-20">
+                    <Col span={8}>
+                        <Row>
+                            <Col span={12}>
+                                <Link>
+                                    <Row gutter={15}>
+                                        <Col className="mt-5">
+                                            <Image width={32} height={32} src={"https://id.fpt.vn/images/Contract32.svg"} />
+                                        </Col>
+                                        <Col>
+                                            Confirmation of <br /> electronic contracts
+                                    </Col>
+                                    </Row>
+                                </Link>
+                            </Col>
+                            <Col span={12}>
+                                <Link>
+                                    <Row gutter={15}>
+                                        <Col className="mt-5">
+                                            <Image width={32} height={32} src={"https://id.fpt.vn/images/Loyalty40.svg"} />
+                                        </Col>
+                                        <Col>
+                                            Look up <br /> for acceptance
+                                        </Col>
+                                    </Row>
+                                </Link>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+                <Row className="mt-25 mb-250">
+                    <Col span={8} offset={2}>
+                        <Button icon={<BookTwoTone />} size={"large"} shape={"round"}>User manual</Button>
+                    </Col>
                 </Row>
             </Col>
         </Row>
